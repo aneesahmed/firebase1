13e43ee738667e53bbb1e01aba4b291995a0f413
Anees Ahmed <anahmed@etilizepak.com>
2017-04-16 17:50:08 +0500

    after completing insert and select in list

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-var mainText = document.getElementById("mainText")
-var sumitBtn = document.getElementById("submitBtn")
+var userId = document.getElementById("id")
+var userName = document.getElementById("name")
 
+var mainobj = document.getElementById("mainobj")
 function submitClick() {
-  window.alert("ting");
+  var firebaseRef = firebase.database().ref('/users');
+  var uid= userId.value;
+  var uname = userName.value;
+  //firebaseRef.push().set(msgText);
+  firebaseRef.push().set({
+    id: uid,
+    name: uname
+  });
+  //window.alert("ting");
 }
